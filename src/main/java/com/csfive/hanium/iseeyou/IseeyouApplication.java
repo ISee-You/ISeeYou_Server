3cd5111aae979bf52caa50fa13f39425aab9187d
BangKiHyun <rlrlvh@naver.com>
2020-07-15 15:59:26 +0900

    init

---
+++ src/main/java/com/csfive/hanium/iseeyou/IseeyouApplication.java
@@ -0,0 +1,13 @@
+package com.csfive.hanium.iseeyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IseeyouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IseeyouApplication.class, args);
+    }
+
+}
