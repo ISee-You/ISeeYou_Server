bad04782e69bcbbda8f6a3afc77793143f128b82
BangKiHyun <rlrlvh@naver.com>
2020-07-22 15:02:00 +0900

    feat Student

---
+++ src/main/java/com/csfive/hanium/iseeyou/enums/GenderType.java
@@ -0,0 +1,12 @@
+package com.csfive.hanium.iseeyou.enums;
+
+public enum GenderType {
+    MALE("남자"),
+    FEMAIL("여자");
+
+    private final String name;
+
+    GenderType(final String name) {
+        this.name = name;
+    }
+}
