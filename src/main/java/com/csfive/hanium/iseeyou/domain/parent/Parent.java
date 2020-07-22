6a2baed0f07ee8b27c0c574bf6188fbf9be7d0b3
hyunjong-96 <ahajongs@gmail.com>
2020-07-22 15:09:52 +0900

    feat/parent

---
+++ src/main/java/com/csfive/hanium/iseeyou/domain/parent/Parent.java
@@ -0,0 +1,45 @@
+package com.csfive.hanium.iseeyou.domain.parent;
+
+import com.csfive.hanium.iseeyou.domain.student.Student;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Parent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@Column(name = "PARENT_ID")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    //@OneToMany(mappedBy = "parent")
+    //private List<Student> students = new ArrayList<Student>();
+
+    @Builder
+    public Parent(String name, String password, String email, Gender gender){
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.gender = gender;
+    }
+
+}
---
+++ src/main/java/com/csfive/hanium/iseeyou/domain/parent/ParentRepository.java
@@ -0,0 +1,6 @@
+package com.csfive.hanium.iseeyou.domain.parent;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParentRepository extends JpaRepository<Parent, Long> {
+}
