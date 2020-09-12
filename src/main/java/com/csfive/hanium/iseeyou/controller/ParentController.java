2f290b904bb770066456f7bf5360c63df2a97903
hyunjong-96 <ahajongs@gmail.com>
2020-09-13 01:54:36 +0900

    "Feat : add signup validation"

--- src/main/java/com/csfive/hanium/iseeyou/controller/ParentController.java
+++ src/main/java/com/csfive/hanium/iseeyou/controller/ParentController.java
@@ -3,15 +3,20 @@ package com.csfive.hanium.iseeyou.controller;
 import com.csfive.hanium.iseeyou.dto.parent.*;
 import com.csfive.hanium.iseeyou.dto.student.StudentDetailResDto;
 import com.csfive.hanium.iseeyou.service.ParentService;
+import com.csfive.hanium.iseeyou.service.ValidateService;
 import com.csfive.hanium.iseeyou.utils.ErrorException;
 import com.csfive.hanium.iseeyou.utils.StatusCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import sun.reflect.annotation.ExceptionProxy;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Map;
 
 import static com.csfive.hanium.iseeyou.utils.ResponseMessage.*;
 
@@ -21,11 +26,27 @@ import static com.csfive.hanium.iseeyou.utils.ResponseMessage.*;
 public class ParentController {
 
     private final ParentService parentService;
+    private final ValidateService validateService;
 
     @PostMapping("/")
-    public ResponseEntity<String> signup(@RequestBody ParentSavetRequestDto saveRequestDto){
-        parentService.signup(saveRequestDto);
-        return ResponseEntity.ok(CREATE_USER);
+    public ResponseEntity signup(@RequestBody @Valid ParentSavetRequestDto saveRequestDto, Errors errors, Model model){
+        try{
+            if(errors.hasErrors()){
+                model.addAttribute("saveRequestDto",saveRequestDto);
+
+                Map<String, String> validatorResult = validateService.validateHandling(errors);
+                for(String key : validatorResult.keySet()){
+                    model.addAttribute(key, validatorResult.get(key));
+                }
+                return ResponseEntity.status(StatusCode.BAD_REQUEST).body(validatorResult);
+            }
+
+            parentService.signup(saveRequestDto);
+            return ResponseEntity.ok(CREATE_USER);
+        }catch (Exception e){
+            return ResponseEntity.status(StatusCode.BAD_REQUEST).body(e.getMessage());
+        }
+
     }
 
     @PostMapping("/Login")
--- src/main/java/com/csfive/hanium/iseeyou/dto/parent/ParentSavetRequestDto.java
+++ src/main/java/com/csfive/hanium/iseeyou/dto/parent/ParentSavetRequestDto.java
@@ -7,12 +7,22 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class ParentSavetRequestDto {
 
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
+
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식에 맞지 않습니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private String password;
     private GenderType gender;
 
